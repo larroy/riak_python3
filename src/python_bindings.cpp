acb16f4ea8212c45331e34f3025e765ec46704c0
Pedro Larroy <pedro.larroy@gmail.com>
2013-12-17 15:49:52 +0100

    get a basic build going

--- src/python_bindings.cpp
+++ src/python_bindings.cpp
@@ -6,13 +6,11 @@ using namespace boost::python;
 class RiakClient
 {
 public:
-    RiakClient(const std::string& host, const std::string& port)
+    RiakClient(const std::string& host, const std::string& port):
         m_cli(riak::new_client(host, port))
     {
         
     }
-
-    void put(
 
 private:
     riak::client_ptr m_cli;
