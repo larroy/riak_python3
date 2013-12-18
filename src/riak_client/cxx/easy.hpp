9c4918b73ce23f721d1f22708fe19dde4c5ac813
Pedro Larroy <pedro.larroy@gmail.com>
2013-12-18 18:30:28 +0100

    comment unimplemented members

--- src/riak_client/cxx/easy.hpp
+++ src/riak_client/cxx/easy.hpp
@@ -39,40 +39,20 @@ public:
     Client(const std::string& host, const std::string& port);
     ~Client();
 
-    void set_def_metadata(const riak_metadata& metadata)
-    {
-        m_def_metadata = metadata;
-    }
-
-    void set_def_store_params(const store_params& sparams)
-    {
-        m_def_store_params = sparams;
-    }
-
-    void set_def_bucket_properties(const bucket_properties& bprop)
-    {
-        m_def_bucket_properties = bprop;
-    }
-
-    void set_def_r(int r)
-    {
-        m_def_r = r;
-    }
-
     bool ping();
-    void set_client_id(int id);
-    uint32_t get_client_id();
-    string_vector list_buckets();
-    string_vector list_keys(const std::string& bucket);
+    //void set_client_id(int id);
+    //uint32_t get_client_id();
+    //string_vector list_buckets();
+    //string_vector list_keys(const std::string& bucket);
     void put(const std::string& bucket, const std::string& key, const std::string& value);
     void put_params(const std::string& bucket, const std::string& key, const std::string& value, int w, int dw);
 
-    response<result_ptr> fetch(const std::string& bucket, const std::string& key, int r);
+    //response<result_ptr> fetch(const std::string& bucket, const std::string& key, int r);
     std::string fetch_value(const std::string& bucket, const std::string& key);
-    riak_metadata fetch_metadata(const std::string& bucket, const std::string& key, int r);
-    void set_bucket_properties(const std::string& bucket, const bucket_properties& prop);
+    //riak_metadata fetch_metadata(const std::string& bucket, const std::string& key, int r);
+    //void set_bucket_properties(const std::string& bucket, const bucket_properties& prop);
 
-private:
+public:
     std::string m_host;
     std::string m_port;
     client_ptr m_client;
